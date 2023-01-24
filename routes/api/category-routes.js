954cd2e56ce97df5f57cdc37bc671234933d4885
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-23 20:22:00 -0600

    Added home and .id routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,22 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+ Category.findOne({
+  where: {
+    id: req.params.id,
+  },
+  include: [Product],
+ })
+  .then((category) => res.json(category))
+  .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
