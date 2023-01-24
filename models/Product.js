9c44096161183b0c80e2b70c03b176e8ab793db8
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-23 19:49:00 -0600

    Adding to product.js

--- models/Product.js
+++ models/Product.js
@@ -14,7 +14,26 @@ Product.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-    }
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
   },
   {
     sequelize,
